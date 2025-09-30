ea1176805fdad9258a1833f99041163ea837004b
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-09-30 14:28:14 +0100

    Improving projects code and data

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -6,7 +6,7 @@ export interface Project {
     postLink?: string;
     demoLinkRel?: string;
     isUnderConstruction: boolean;
-    publishedLink?: string;
+    publishedPackageLink?: string;
     version?: string;
     isFeatured: boolean;
 
@@ -18,7 +18,7 @@ export const projects: Project[] = [
         name: "Data Seeder for .NET",
         description: "A powerful and flexible data seeding library for .NET applications, designed to streamline the process of populating databases with test or initial data. It supports environment-specific data, dependency management between seeders, batch processing for large datasets, and hash-based change detection. It integrates seamlessly with Entity Framework.",
         demoLink: 'https://github.com/joeloudjinz/InzSeeder',
-        publishedLink: 'https://www.nuget.org/packages/InzSoftwares.NetSeeder/',
+        publishedPackageLink: 'https://www.nuget.org/packages/InzSoftwares.NetSeeder/',
         isUnderConstruction: false,
         isFeatured: true,
         version: '2.0.3',
