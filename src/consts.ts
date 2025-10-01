b30426ca27be2518a5db2f98e0d519f63b9dd404
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-10-01 11:17:52 +0100

    Improving site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@
 
 export const FULL_NAME = 'Abdellah Addoun';
 export const NICKNAME = 'JoeInz';
-export const SITE_TITLE = 'Abdellah addoun';
+export const SITE_TITLE = 'JoeInz Personal Website';
 export const SITE_TAGLINE = 'Software Engineer Personal Website'; // site tagline
-export const SITE_DESCRIPTION = 'My personal website where I share my experience, projects, thoughts, and more.'; // site meta description for SEO
+export const SITE_DESCRIPTION = 'Abdellah ADDOUN\' personal website. Find experience, projects, thoughts, and more about the senior software engineer with the name JoeInz here, welcome.';
 export const EMAIL = 'addoun.abdellah@gmail.com';
 
 export const GITHUB_ACCOUNT = 'https://github.com/joeloudjinz';
